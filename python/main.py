9669056ddf58ae2895a0eceb8cc6b12bc41e397c
Martin Nadal <martin.nadal@gmail.com>
2019-08-31 22:05:37 +0200

    opening feed and liking Instagram

--- python/main.py
+++ python/main.py
@@ -1,6 +1,8 @@
 import os
 import re
 from time import sleep
+import xml.etree.ElementTree as ET
+import random
 
 apps = {'GoogleMaps':('com.google.android.apps.maps','com.google.android.maps.MapsActivity'),
 		'Instagram':('com.instagram.android','com.instagram.mainactivity.MainActivity')}
@@ -25,13 +27,35 @@ def pressKey(keyCode):
 
 def insertText(text):
     """insert text as if it was inserted from an external keyboard"""
-    sendAdb('input  keyboard text \"{}\"'.format(text),True)
+    sendAdb('input  keyboard text \"{}\"'.format(text))
 
 def getScreenSize():
     """return screen size in a tuple"""
     output  = sendAdb('wm size')
     m = re.search(r'.*:\s+([0-9]+)x([0-9]+)', output)
     return (int(m.group(1)), int(m.group(2)))
+
+def getXMLUI():
+    """get the UI in XML format"""
+    output  = sendAdb('uiautomator dump')
+
+    m = re.search(r'(\S+.xml)', output)
+    if m == None:
+        print('error')
+        return None
+    filepath = m.group(1)
+    xmlstr = sendAdb('cat {}'.format(filepath))
+    root = ET.fromstring(xmlstr)
+    return root
+
+def extractBounds(xmlElement):
+    
+    bounds = xmlElement.attrib['bounds']
+    print(bounds)
+    m = re.search(r'\[([0-9]+),([0-9]+)\]',bounds)
+    return (int(m.group(1)),int(m.group(2)))
+
+
 
 def openURL(url):
 	"""open URL with the default browser"""
@@ -56,6 +80,8 @@ def getDump(subsytem,term = None):
 		lines = list(filter(lambda l:term in l,lines))
 	return lines
 
+
+
 def isSuspended():
 	"""Check if it is suspended (black screen)"""
 	return getDump('power','mHoldingDisplaySuspendBlocker=true')==[]
@@ -67,7 +93,7 @@ def isLocked():
 def unlock(PIN = None):
     """unlock phone, swipes from bottom to the middle of the screen + PIN + enter"""
     pressKey(26)
-    sleep(0.500)
+    sleep(1)
     if isSuspended():
         pressKey(26)
     (w,h) = getScreenSize()  
@@ -84,5 +110,37 @@ if __name__ == "__main__":
         print('connected')
     else:
         print('disconnected')
-    print(isLocked())
-    unlock()
+    
+    (w,h) = getScreenSize()  
+
+    print(isSuspended())
+    unlock(2001)
+    launchActivity(*apps['Instagram'])
+    root = getXMLUI()
+    
+
+    #explore
+    buttons = root.findall(".//node[@resource-id='com.instagram.android:id/tab_icon']")
+    tap(*extractBounds(buttons[1]))
+
+    #click in a random image from explore 
+    
+    for i in range(random.randint(0,4)):
+        swipe(w/2,3*h/4,w/2,h/4)
+
+    root = getXMLUI()
+    images = root.findall(".//node[@class='android.widget.ImageView'][@resource-id='']")
+    tap(*extractBounds(random.choice(images)))
+    print('feed')
+    
+    for i in range(10):
+       
+        swipe(w/2,3*h/4,w/2,h/4)
+        sleep(.5)
+        root = getXMLUI()
+        hearts = root.findall(".//node[@resource-id='com.instagram.android:id/row_feed_button_like']")
+        if len(hearts) > 0:
+            heart = random.choice(hearts)
+            tap(*extractBounds(heart))
+            tap(*extractBounds(heart))
+            
