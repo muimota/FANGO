15eef88d8341469c581a71cc6fde8931dca03924
Martin Nadal <martin.nadal@gmail.com>
2019-10-15 17:12:52 +0200

    amazon and instagram more stable

--- python/amazon.py
+++ python/amazon.py
@@ -1,44 +1,93 @@
 from fangolib import *
 from time import sleep
 import random
-
-def Amazon():
-
+import uiautomator2 as ui2
+import glob
+import re
+def Amazon(loops = 6):
+    
     (w,h) = getScreenSize()
-
-    alphabet = ('abcdefghijklmnopqrstuvwxyz')
+    d = ui2.connect()
+    images = sorted(glob.glob('screens/*.png'))
+    
+    if len(images) > 0:
+        lastimage = images[-1]
+        imageIndex = int(re.match(r'.*/([0-9]+)\.png',lastimage).group(1))
+    else:
+        imageIndex = 0
+    
     launchActivity(*apps['Amazon'])
-    pressKey(84)
-    root = getXMLUI()
-    textField = root.find(".//node[@resource-id='com.amazon.mShop.android.shopping:id/rs_search_src_text']")
-    tap(*getCenter(textField))
-    root = getXMLUI()
-    clearSearch = root.find(".//node[@resource-id='com.amazon.mShop.android.shopping:id/rs_clear_text_button_accessibility']")
     
-    if clearSearch != None:
-        tap(*getCenter(clearSearch))
-        print('clear text')
-    
-    insertText(random.choice(alphabet))
-    insertText(random.choice(alphabet))
-    sleep(1)
-    clearSuggestion = root.findall(".//node[@resource-id='com.amazon.mShop.android.shopping:id/iss_search_dropdown_item_clear']")
-    suggCount = len(clearSuggestion)
-    print('previous Suggestions: {}'.format(suggCount))
-    for i in range(suggCount):
-        tap(*getCenter(clearSuggestion[0]))
-        sleep(.2)   
-    sleep(2)
-    root = getXMLUI()
-    suggNodes = root.findall(".//node[@resource-id='com.amazon.mShop.android.shopping:id/iss_search_dropdown_item_text']")
-    suggestions = [suggNode.attrib['text'] for suggNode in suggNodes]
-    if len(suggestions) > 0:
-        suggestion = random.choice(suggestions)
-        insertText(suggestion[2:])
-        pressKey(66)
-    
-    sleep(3)
-    swipe(w/2,h/2,w/2,0)
-    sleep(1)
-    tap(w/2,h/2)
-    sleep(8)
+    for i in range(loops):
+        alphabet = ('abcdefghijklmnopqrstuvwxyz')
+        
+        pressKey(84)
+        root = getXMLUI(device = d)
+        textField = root.find(".//node[@resource-id='com.amazon.mShop.android.shopping:id/rs_search_src_text']")
+        tap(*getCenter(textField))
+        root = getXMLUI(device = d)
+        clearSearch = root.find(".//node[@resource-id='com.amazon.mShop.android.shopping:id/rs_clear_text_button_accessibility']")
+        
+        if clearSearch != None:
+            tap(*getCenter(clearSearch))
+            print('clear text')
+        
+        insertText(random.choice(alphabet))
+        insertText(random.choice(alphabet))
+        #insertText('er')
+        
+        sleep(.2)
+        root = getXMLUI(device = d)
+        clearSuggestion = root.findall(".//node[@resource-id='com.amazon.mShop.android.shopping:id/iss_search_dropdown_item_clear']")
+        suggCount = len(clearSuggestion)
+        print('previous Suggestions: {}'.format(suggCount))
+        sleep(1)
+        for i in range(suggCount):
+            tap(*getCenter(clearSuggestion[0]))
+            sleep(.2)   
+        sleep(1)
+        
+        root = getXMLUI(device = d)
+        suggNodes = root.findall(".//node[@resource-id='com.amazon.mShop.android.shopping:id/iss_search_dropdown_item_text']")
+        suggestions = [suggNode.attrib['text'] for suggNode in suggNodes]
+        print(suggestions)
+        if len(suggestions) > 0:
+            suggestion = random.choice(suggestions)
+            print(suggestion)
+            insertText(suggestion[2:])
+            sleep(1)
+            pressKey(66)
+            
+        sleep(2)
+        swipe(w/2,h/2,w/2,0,random.randint(300,600))
+        sleep(1)
+        root = getXMLUI(device = d)
+        views = root.findall(".//node[@class='android.view.View']")
+        products = [view for view in views if len(view.attrib['text'].strip()) > 0 ]
+        if len(products) > 0:
+            product = random.choice(products)
+            print('product:{}'.format(product.attrib['text']))
+            tap(*getCenter(product))
+        else:
+            print('no product found, tap in center')
+            tap(w/2,h/2)
+            
+        sleep(6)
+        imageIndex += 1
+        filename = 'screens/{:06}.png'.format(imageIndex)
+        screenshot(filename)
+        print('save screenshot:{}'.format(filename))
+        #multi image
+        root = getXMLUI(device = d)
+        pagination_block = root.find(".//node[@resource-id='image-block-pagination-dots']")
+        if pagination_block != None:
+            pages_count = max([int(node.attrib['index']) for node in pagination_block.iter()])
+            image_block = root.find(".//node[@resource-id='image-block-row']")
+            centerImage = getCenter(image_block)
+            for i in range(pages_count - 1):
+                swipe(centerImage[0] + w/4 ,centerImage[1],centerImage[0] -  + w/4 ,centerImage[1])
+                sleep(1)
+            
+
+        
+        
--- python/instagram.py
+++ python/instagram.py
@@ -1,34 +1,44 @@
 from fangolib import *
 from time import sleep
 import random
+import uiautomator2 as ui2
 
 def Instagram(loops = 1):
 
     (w,h) = getScreenSize()
+    d = ui2.connect()
     
     for i in range(loops):
 
         buttons = []
 
         while buttons == [] :
-            root = getXMLUI()
+            
+            
             try:
                 launchActivity(*apps['Instagram'])
             except:
+                print('exception')
                 pass
+            root = getXMLUI(device=d)
+            sleep(2)
             
             buttons = root.findall(".//node[@resource-id='com.instagram.android:id/tab_icon']")
             if buttons == []:
-                pressKey(4)
+                print('no tab')
+                continue
         
         tap(*getCenter(buttons[1]))
-
+        sleep(.4)
+        tap(*getCenter(buttons[1]))
+        swipe(w/2,h/4,w/2,h/2)
+        sleep(.7)
         #click in a random image from explore
 
-        for i in range(random.randint(1,4)):
+        for i in range(random.randint(0,4)):
             swipe(w/2,3*h/4,w/2,h/4)
 
-        root = getXMLUI()
+        root = getXMLUI(device=d)
         images = root.findall(".//node[@class='android.widget.ImageView'][@resource-id='com.instagram.android:id/image_button']")
         index = random.randint(0,len(images)-1)
         index = 0
@@ -41,13 +51,28 @@ def Instagram(loops = 1):
         print('feed')
 
         for i in range(random.randint(1,10)):
-
-            swipe(w/2,3*h/4,w/2,h/4,random.randint(300,1000))
             
-            root = getXMLUI()
+            swipe(w/2,3*h/4,w/2,h/4,random.randint(300,1000))
+            sleep(.5)
+            root = getXMLUI(device=d)
+            #detect if there is a multiple photograph
+            multi = root.find(".//node[@resource-id='com.instagram.android:id/carousel_bumping_text_indicator']")
+            if multi != None:
+                print('multiimage')
+                pages = int(multi.attrib['text'].split('/')[1])
+                swipecoords = getCenter(multi)
+                swipe(swipecoords[0],swipecoords[1],swipecoords[0],h/5,1000)
+                
+                for i in range(pages):
+                    swipe(swipecoords[0],h/5,0,h/5,1000)
+                    sleep(.5)
+                root = getXMLUI(device=d)
+            
+            #detect hearts
             hearts = root.findall(".//node[@resource-id='com.instagram.android:id/row_feed_button_like']")
             if len(hearts) > 0:
                 heart = random.choice(hearts)
+                print('like!')
                 tap(*getCenter(heart))
                 sleep(.15)
                 tap(*getCenter(heart))
