deb62d0dfbb53bde7f0302963e19fe871c381fe1
Martin Nadal <martin.nadal@gmail.com>
2020-11-10 21:51:45 +0100

    random search

--- python/instagram.py
+++ python/instagram.py
@@ -5,6 +5,8 @@ import uiautomator2 as ui2
 
 def Instagram(loops = 1):
 
+    alphabet = ('abcdefghijklmnopqrstuvwxyz')
+    
     (w,h) = getScreenSize()
     d = ui2.connect()
     
@@ -21,8 +23,11 @@ def Instagram(loops = 1):
             
         sleep(2)
         print('capture xml')
+
         root = getXMLUI(device=d)
 
+        #TODO:check is we are ina Comments page
+        
         print('home menu')
         buttons = root.findall(".//node[@content-desc='Home']")
         print('{} buttons found'.format(len(buttons)))
@@ -31,25 +36,43 @@ def Instagram(loops = 1):
          
         tap(*getCenter(button))
         sleep(.4)
-        
+
+        root = getXMLUI(device=d)
         button = root.find(".//node[@content-desc='Search and Explore']")
         tap(*getCenter(button))
         sleep(.4)
+
+        root = getXMLUI(device=d)
+        t = root.find('.//node[@resource-id="com.instagram.android:id/action_bar_search_edit_text"]')
+        sleep(.4)
+        tap(*getCenter(t))
+        insertText('#')
+        insertText(random.choice(alphabet))
+        insertText(random.choice(alphabet))
+        sleep(1)
+        swipe(w/2,h/2,w/2,h/6)
+              
+        #suggestion
+        sleep(1)
+        root = getXMLUI(device=d)
+        suggestions = root.findall('.//node[@resource-id="com.instagram.android:id/row_hashtag_textview_tag_name"]')
        
+        tap(*getCenter(random.choice(suggestions)))
+        sleep(10)     	 
         #click in a random image from explore
 
         #for i in range(random.randint(0,4)):
         #    swipe(w/2,3*h/4,w/2,h/4)
-
+        
         root = getXMLUI(device=d)
         images = root.findall(".//node[@class='android.widget.ImageView'][@resource-id='com.instagram.android:id/image_button']")
-        index = random.randint(0,len(images)-1)
-        index = 0
+        print("found {} images".format(len(images)))
         #sleep(.5)
         clickableImages = [image for image in images if getCenter(image)[1] > h/5 or getCenter(image)[1] > 4*h/5]
+        index = random.randint(0,len(clickableImages)-1)
         bounds = getCenter(clickableImages[index])
-
-        print("total:{} selected:{} bounds:{} desc:{}".format(len(images),index,bounds,images[index].attrib['content-desc']))
+        
+        print("total:{} selected:{} bounds:{} desc:{}".format(len(clickableImages),clickableImages,bounds,clickableImages[index].attrib['content-desc']))
         tap(*bounds)
         print('feed')
 
